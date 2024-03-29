b8ddc07679e8472ecfc2ae3590903c43d7a8b83c
richwrd <dudurichard07@gmail.com>
2024-03-28 22:20:16 -0300

    AAAAAAAAAAA

--- SEMESTRE_5/PROGAMACAO_WEB/api_empresa_sequelize/routes/usersRoutes.js
+++ SEMESTRE_5/PROGAMACAO_WEB/api_empresa_sequelize/routes/usersRoutes.js
@@ -7,6 +7,7 @@ router.get('/list', usersController.listUser)
 // router.post('/all_list', usersController.listUser)
 router.get('/create', usersController.createUser) 
 router.post('/create', usersController.createUserPost)
+
 router.get('/edit/:id', usersController.editUser)
 router.post('/edit', usersController.editUserPost)
 
--- SEMESTRE_5/PROGAMACAO_WEB/api_produtos_mongoose/models/Product.js
+++ SEMESTRE_5/PROGAMACAO_WEB/api_produtos_mongoose/models/Product.js
@@ -2,12 +2,12 @@ const mongoose = require('../db/conn')
 const { Schema } = mongoose
 
 const Product = mongoose.model(
-    'products',
+    'produtos',
     new Schema({ //campos da collection
         name: { type: String, required: true },
         price: { type: Number, required: true },
         description: { type: String, required: true },
-        image : { type: String, required: true},
+        image : { type: String, required: true },
     })
     )
 
